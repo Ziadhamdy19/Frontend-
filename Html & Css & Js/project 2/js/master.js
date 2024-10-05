058b95674bd351268ee8beaac68ec8e13b57e828
ziad <ziadhamdy19920@gmail.com>
2024-10-05 15:42:42 +0300

    upload

---
+++ Html & Css & Js/project 2/js/master.js
@@ -0,0 +1,8 @@
+let mainImage = document.getElementById('mainImage');
+
+function make(src){
+    mainImage.src = src;
+}
+function colors(color){
+    document.body.style.backgroundColor = color;
+}
